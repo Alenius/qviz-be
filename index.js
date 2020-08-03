acfad9d03648f6c1a9e3680af2dc480c6358c3be
Adam Alenius <adam.alenius@gmail.com>
2020-08-03 22:02:10 +0200

    start setting upp fuzzy matching for answers

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const port = 4000;
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 4000
 
-const queries = require("./db/queries");
-const { request } = require("express");
+const queries = require('./db/queries')
+const { request } = require('express')
 
 var corsOptions = {
-  origin: "http://localhost:3000",
-};
+  origin: 'http://localhost:3000',
+}
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-);
+)
 
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-app.get("/questions", async (request, response) => {
-  console.log({ param: request.query.id });
-  const questionId = request.query.id;
-  const allQuestions = await queries.getQuestions(questionId);
-  console.log({ allQuestions: allQuestions[0].questions });
-  response.json({ questions: allQuestions });
-});
+app.get('/questions', async (request, response) => {
+  console.log({ param: request.query.id })
+  const questionId = request.query.id
+  const allQuestions = await queries.getQuestions(questionId)
+  console.log({ allQuestions: allQuestions[0].questions })
+  response.json({ questions: allQuestions })
+})
+
+app.get('/answer', async (request, response) => {
+  console.log({ param: request.query })
+  const { quizId, questionId, userAnswer } = request.query
+  const {
+    correctAnswer,
+    userAnswerWasCorrect,
+  } = await queries.checkAnswerForQuestion({ quizId, questionId, userAnswer })
+  response.json({ userAnswer, correctAnswer, userAnswerWasCorrect })
+})
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
-});
+  console.log(`App running on port ${port}.`)
+})
