5c849d54d896658815ce7fcb4479625afd28861a
Adam Alenius <adam.alenius@gmail.com>
2020-08-03 20:16:32 +0200

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
 const queries = require("./db/queries");
+const { request } = require("express");
 
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
