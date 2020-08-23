726fb9995e93a9172dc51e7e90a1b297e8f39d98
Adam Alenius <adam.alenius@gmail.com>
2020-08-23 18:43:20 +0200

    refactor: change incorrect comment about accepted fuzzy match limit

--- src/services/checkAnswer.js
+++ src/services/checkAnswer.js
@@ -8,7 +8,7 @@ const checkAnswer = async ({ quizId, questionId, userAnswer }) => {
   const fs = FuzzySet(possibleAnswers)
   const fuzzyMatch = fs.get(userAnswer)
 
-  // accept answer if any has 0.6 or higher
+  // accept answer if any has 0.75 or higher
   const isAnswerAcceptable = pipe(
     filter((it) => head(it) > 0.75),
     length,
