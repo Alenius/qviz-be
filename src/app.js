5f44e9ea9be110bfaf9cd27a69b36cb1100b8594
Adam Alenius <adam.alenius@gmail.com>
2020-08-29 16:49:36 +0200

    fix: add applying of middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const { connectRoutes } = require('./api')
+const { applyMiddlewares } = require('./api/middlewares')
 const port = 4000
 
 const startServer = () => {
   const app = express()
+  applyMiddlewares(app)
 
   const router = connectRoutes()
   app.use('/', router)
