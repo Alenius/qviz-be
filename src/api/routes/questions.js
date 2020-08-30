29846b7469f05a4a010299fb915383a30f8208d0
Adam Alenius <adam.alenius@gmail.com>
2020-08-30 14:46:53 +0200

    feat: redo the endpoint for creating questions into accepting an array of questions instead of a single question

--- src/api/routes/questions.js
+++ src/api/routes/questions.js
@@ -1,6 +1,7 @@
 const { getAllQuestions } = require('../../services/getAllQuestionsFromQuiz')
 const { insertQuestionAndAnswer } = require('../../db/queries')
 const Joi = require('joi')
+const { forEach } = require('ramda')
 
 const connectQuestionRoutes = async (router) => {
   router.get('/questions', async (request, response) => {
@@ -14,25 +15,39 @@ const connectQuestionRoutes = async (router) => {
 
   router.post('/questions', async (request, response) => {
     const { value, error: validationError } = Joi.object({
-      questionText: Joi.string().required(),
-      quizId: Joi.string().required(),
-      acceptedAnswers: Joi.string().required(),
-      extraInfo: Joi.string(),
+      questions: Joi.array()
+        .items(
+          Joi.object({
+            questionText: Joi.string().required(),
+            quizId: Joi.string().required(),
+            acceptedAnswers: Joi.string().required(),
+            extraInfo: Joi.string(),
+          })
+        )
+        .required(),
     }).validate(request.body)
 
     if (validationError) {
       return response.status(500).send(String(validationError))
     }
 
-    const { questionText, quizId, acceptedAnswers, extraInfo = '' } = value
+    const { questions } = value
 
     try {
-      await insertQuestionAndAnswer(
-        quizId,
-        questionText,
-        acceptedAnswers,
-        extraInfo
-      )
+      forEach(async (question) => {
+        const {
+          questionText,
+          quizId,
+          acceptedAnswers,
+          extraInfo = '',
+        } = question
+        await insertQuestionAndAnswer(
+          quizId,
+          questionText,
+          acceptedAnswers,
+          extraInfo
+        )
+      }, questions)
       response.send('Question and answer inserted into the database')
     } catch (err) {
       console.log(err)
