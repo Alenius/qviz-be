5b7e2b5fbd09b8b1e70b460436c2eeab0f04dd64
Adam Alenius <adam.alenius@gmail.com>
2020-12-03 21:07:20 +0100

    feat: add DELETE method to the quiz endpoint

--- src/api/routes/quiz.js
+++ src/api/routes/quiz.js
@@ -4,6 +4,8 @@ const {
   getAllQuizzesByQuizName,
   getAllQuizzesByAuthor,
   getAllQuizzes,
+  deleteQuiz,
+  getQuizById,
 } = require('../../db/queries')
 const Joi = require('joi')
 
@@ -43,7 +45,6 @@ const connectQuizRoutes = async (router) => {
   })
 
   router.post('/quiz', async (request, response) => {
-    console.log({ body: request.body })
     const { value, error: validationError } = Joi.object({
       quizName: Joi.string().required(),
       author: Joi.string().required(),
@@ -74,6 +75,30 @@ const connectQuizRoutes = async (router) => {
       response
         .status(500)
         .send('Something went wrong when writing to the database')
+    }
+  })
+
+  router.delete('/quiz', async (request, response) => {
+    const { value, error: validationError } = Joi.object({
+      quizId: Joi.number().required(),
+    }).validate(request.body)
+
+    if (validationError) {
+      return response.status(500).send(String(validationError))
+    }
+
+    const { quizId } = value
+
+    try {
+      const exists = (await getQuizById(quizId)).length !== 0
+      if (!exists) {
+        response.status(404).send('No quiz with that id found')
+      }
+      await deleteQuiz(quizId)
+      response.status(200).send({ quizId })
+    } catch (err) {
+      console.log(err)
+      response.status(500).send('Something went wrong when deleting the quiz')
     }
   })
 }
