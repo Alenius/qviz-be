42e7a44e38630d5f60736fc17a8b5396a0fbe275
Adam Alenius <adam.alenius@gmail.com>
2021-02-15 21:13:57 +0100

    wip: start adding auth middleware

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,9 +1,28 @@
-import { Application } from "express"
+import { Application, RequestHandler } from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
 var corsOptions = {
   origin: ['http://localhost:3000', 'https://qviz-game.herokuapp.com'],
+}
+
+export const auth: RequestHandler = (req, res, next) => {
+  // get token from header if present
+  const token = req.headers['x-access-token'] || req.headers['authorization']
+  if (!token) {
+    return res.status(401).send('No auth token found. Access denied.')
+  }
+
+  try {
+    const privateKey: jwt.Secret = process.env.AUTH_PRIVATE_KEY ?? ''
+    if (!privateKey) throw Error('No private key')
+    const decoded = jwt.verify(token as string, privateKey)
+    req.user = decoded as string
+    next()
+  } catch (err) {
+    res.status(400).send('Invalid token')
+  }
 }
 
 export const applyMiddlewares = (app: Application) => {
--- src/api/routes/questions.ts
+++ src/api/routes/questions.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { getQuestions } from '../handlers'
+import { auth } from '../middlewares'
 
 export const connectQuestionRoutes = async (router: Router) => {
-  router.get('/questions', getQuestions)
+  router.get('/questions', [auth], getQuestions)
 }
