1c1613830d7eea300569fd1d0363dcc29162b2a9
Adam Alenius <adam.alenius@gmail.com>
2021-02-22 21:14:47 +0100

    add password for the users up migration

--- migrations/sqls/20210221183755-add-users-up.sql
+++ migrations/sqls/20210221183755-add-users-up.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
   user_id    uuid      DEFAULT   uuid_generate_v4 (),
   first_name VARCHAR,
   last_name  VARCHAR,
-  username  VARCHAR   NOT NULL,
+  username  VARCHAR   NOT NULL UNIQUE,
+  password  VARCHAR   NOT NULL,
   PRIMARY KEY(user_id)
 );
