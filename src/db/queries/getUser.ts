962881cd95442b1d3047c9e203d3701b059b6630
Adam Alenius <adam.alenius@gmail.com>
2021-03-02 20:36:29 +0100

    fix user query return values

--- src/db/queries/getUser.ts
+++ src/db/queries/getUser.ts
@@ -3,7 +3,7 @@ import { QvizDB, User } from '../../../types'
 export const getUser = (db: QvizDB) => async (username: string) =>
   db.query<User>(`
     SELECT
-      * 
+     user_id AS "userId", username, password
     FROM
       users
     WHERE
